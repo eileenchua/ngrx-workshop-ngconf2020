426e5c52da200634496e74ad65cb7bc637708205
Eileen Chua <eileenchua926@gmail.com>
2020-08-30 14:51:31 +0800

    [03-reducers] Add create, update, and delete functions for book reducer
    Missed out in the workshop.

--- src/app/shared/state/books.reducer.ts
+++ src/app/shared/state/books.reducer.ts
@@ -2,6 +2,14 @@ import { createReducer, on, Action, createSelector } from "@ngrx/store";
 import { BookModel, calculateBooksGrossEarnings } from "src/app/shared/models";
 import { BooksPageActions, BooksApiActions } from "src/app/books/actions";
 
+const createBook = (books: BookModel[], book: BookModel) => [...books, book];
+const updateBook = (books: BookModel[], changes: BookModel) =>
+  books.map((book) => {
+    return book.id === changes.id ? Object.assign({}, book, changes) : book;
+  });
+const deleteBook = (books: BookModel[], bookId: string) =>
+  books.filter((book) => bookId !== book.id);
+
 // Keep track of books as well as the book selected
 export interface State {
   collection: BookModel[];
